81d54c64971317282ca338fa24fc987ea80b804f
mikkeldamm <mikkeldamm@hotmail.com>
2021-06-28 23:48:46 +0200

    First text

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,15 @@ export default function Home() {
       <Head>
         <title>Mikkel Damm - Portfolio</title>
       </Head>
-      <main className="p-10">
-        <h1 className="flex text-7xl">
-          <span role="img" aria-label="Hello" className="block animate-wiggle origin-[70%]">
-            👋🏻
-          </span>
-        </h1>
+      <main>
+        <div className="container m-auto">
+          <h1 className="flex text-3xl font-semibold">
+            <span role="img" aria-label="Hello" className="block animate-wiggle origin-[70%] mr-4">
+              👋🏻
+            </span>
+            I&apos;m Mikkel Damm a developer, entrepreneur and creative guy from Denmark
+          </h1>
+        </div>
       </main>
     </MainLayout>
   );
